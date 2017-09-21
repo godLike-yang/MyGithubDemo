3f52bafd0a9a5834c678631205bbeb8205a2d684
lyzhao <lyzhao@github.com>
2017-09-21 11:18:05 +0800

    代码提交

---
+++ MyGithubDemo/MyGithubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGithubDemo
+//
+//  Created by apple on 2017/9/21.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGithubDemo/MyGithubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGithubDemo
+//
+//  Created by apple on 2017/9/21.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
